2ba16382000a96482c0600b5f1d0ace3f0079f3b
Lam <lamdayap@gmail.com>
2021-02-15 17:06:43 +1100

    updated endpoint to https

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const key = "1bcf4e4f9e57cdb673a9e60e43a54268";
 async function getTemp() {
     try {
         const search = document.getElementById("search");
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${search.value},au&units=metric&appid=${key}`, {mode: 'cors'})
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value},au&units=metric&appid=${key}`, {mode: 'cors'})
         const currentTemp = await response.json();
         console.log(currentTemp)
         temp.innerHTML = currentTemp.main.temp;
